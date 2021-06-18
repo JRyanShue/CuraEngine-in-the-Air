cbad2de573eca6cf86d044f86d9831924828602a
JRyanShue <80791522+JRyanShue@users.noreply.github.com>
2021-06-18 12:41:00 -0400

    Bug fix

--- cli_commands.py
+++ cli_commands.py
@@ -5,7 +5,7 @@ def test_gcode(output):
     # run a test slicing program
     # app (containing the python app) is the directory when run
     print("Running slicer.")
-    subprocess.run("cd .. && cd /ZengerEngine/build && "
+    subprocess.run("cd .. && cd ./ZengerEngine/build && "
         "./CuraEngine slice -p -j /app/ZengerEngine-Presets/definitions/fdmprinter.def.json -j /app/ZengerEngine-Presets/definitions/creality_ender3.def.json -l /app/Test-STLs/5mm_Cube.stl -o " + output
         , shell=True)
     print("Ran test gcode.")
--- main.py
+++ main.py
@@ -23,5 +23,5 @@ def get_gcode():
 
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=5000)
-   print("Web app running.")
+    app.run(host='0.0.0.0', port=5000)
+    print("Web app running.")
