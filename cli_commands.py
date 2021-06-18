387bfa08c897b3c967b0948bf30afab31393288e
JRyanShue <80791522+JRyanShue@users.noreply.github.com>
2021-06-18 13:43:10 -0400

    Added STL path specification

--- cli_commands.py
+++ cli_commands.py
@@ -1,11 +1,11 @@
 import subprocess
 print("Got subprocess import.")
 
-def test_gcode(output):
+def test_gcode(input, output):
     # run a test slicing program
     # app (containing the python app) is the directory when run
     print("Running slicer.")
     subprocess.run("cd .. && cd ./ZengerEngine/build && "
-        "./CuraEngine slice -p -j /root/app/ZengerEngine-Presets/definitions/fdmprinter.def.json -j /root/app/ZengerEngine-Presets/definitions/creality_ender3.def.json -l /root/app/Test-STLs/5mm_Cube.stl -o " + output
+        "./CuraEngine slice -p -j /root/app/ZengerEngine-Presets/definitions/fdmprinter.def.json -j /root/app/ZengerEngine-Presets/definitions/creality_ender3.def.json -l " + input + " -o " + output
         , shell=True)
     print("Ran test gcode.")
--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import subprocess
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 import cli_commands
 
 # Initialize the Flask app
 app = Flask(__name__)
 print("Flask app initialized.")
 
+# initiate class var: STL path
+STL_path = "/root/app/Test-STLs/5mm_Cube.stl"
+
+
 # main page
 @app.route('/')
 def main():
+    if request.method == 'POST':
+        global STL_path
+        STL_path = request.form.get('STL_path')  # access STL_path from form
     return render_template('main.html')
+    
 
 # get gcode
 @app.route('/get_gcode')
 def get_gcode():
-    cli_commands.test_gcode(output="/root/app/test.gcode") # output: app folder in the root directory
+    global STL_path
+    cli_commands.test_gcode(input=STL_path, output="/root/app/test.gcode") # output: app folder in the root directory
     # return gcode (proof of concept)
     with open("/root/app/test.gcode", "r") as f:
         data = f.read()
